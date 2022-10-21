b7f8a08d68f76b1c576b14b1bc665c48e1657a79
mohali4 <mahmohamad560@gmail.com>
2022-10-21 16:11:29 +0330

    add installation tools

---
+++ setup.py
@@ -0,0 +1,27 @@
+from setuptools import setup
+
+
+setup(
+    name='finsh',
+    version='0.0.0',    
+    description='Share files using django',
+    url='https://github.com/mohali4/finsh',
+    author='mohali hamilton',
+    author_email='mahmohamad560@gmail.com',
+    license='MIT',
+    packages=['finsh'],
+    install_requires=['django'],
+                    
+
+    #classifiers=[
+    #    'Development Status :: 1 - Planning',
+    #    'Intended Audience :: Science/Research',
+    #    'License :: OSI Approved :: BSD License',  
+    #    'Operating System :: POSIX :: Linux',        
+    #    'Programming Language :: Python :: 2',
+    #    'Programming Language :: Python :: 2.7',
+    #    'Programming Language :: Python :: 3',
+    #    'Programming Language :: Python :: 3.4',
+    #    'Programming Language :: Python :: 3.5',
+    #],
+)
